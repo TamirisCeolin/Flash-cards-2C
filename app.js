0c98fe97b5259f17c6e3f1f9c3da0c89dfe94164
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-14 14:14:56 -0300

    14/11

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.className = 'cartao'; 
+    cartao.className = 'cartao';
 
     cartao.innerHTML = ` 
                 <div class="cartao__conteudo">
@@ -14,6 +14,16 @@ function criaCartao(categoria, pergunta, resposta){
                     </div>
                 </div>
                  `
+
+    let respostaEstaVisivel = false;
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener('click', viraCartao);
+
     container.appendChild(cartao);
 }
 
